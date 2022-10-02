133838a2e27ec02dc1b181ecbb7a3ed874a693a7
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-10-02 00:58:00 -0300

    Login route created

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 const routes: Routes = [ 
+  {path:'login', component:LoginComponent },
   {
     path:'', component:  NavComponent, children: [
       {path:'home', component: HomeComponent},
