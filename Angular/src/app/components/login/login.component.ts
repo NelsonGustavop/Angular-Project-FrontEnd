ad47213f79d5483f59c5b4ce728a20c985e91458
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-10-02 01:53:21 -0300

    Email validation

--- Angular/src/app/components/login/login.component.ts
+++ Angular/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from "src/app/models/Credenciais.1";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  creds: Credenciais = {
+    email:'',
+    senha:''
+  }
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(8));
   constructor() { }
 
   ngOnInit(): void {
   }
+    validaCampos(): boolean{
+      if(this.email.valid && this.senha.valid){
+        return true;
+      }else{
+        return false;
+      }
+    }
 
 }
