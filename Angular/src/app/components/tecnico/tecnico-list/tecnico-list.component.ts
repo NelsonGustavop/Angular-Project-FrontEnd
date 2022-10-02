317e8c782eab0f989f2162ad0354d4dc5b84dd8e
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-10-02 00:28:09 -0300

    Tecnico-list created

--- Angular/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ Angular/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  ELEMENT_DATA:Tecnico[] = [
+    {
+      id:1,
+      name:'Nelson Yeger',
+      cpf: '321.456.654-23',
+      email:'nelsonYeger@gmail.com',
+      senha:'nelson1234',
+      perfis:['0'],
+      dataCriacao:'26/10/2004'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','Actions'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+
 }
+
